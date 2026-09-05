fd8499df7802c4b48c1aeeeafb6599b8e712a3f7
aymanjameel60 <95094618+aymanjameel60@users.noreply.github.com>
2026-09-06 00:06:21 +0300

    Load unified Spike vendor system UI

--- templates/basic/apps/vendor/src/main.tsx
+++ templates/basic/apps/vendor/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@mercurjs/vendor/index.css";
 import "./spike/vendor-spike.css";
+import "./spike/spike-system-ui.css";
 import App from "@mercurjs/vendor";
 import ExchangeBar from "./spike/ExchangeBar";
 
