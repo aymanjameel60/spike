a5f86508ac4871b2bd70a3de8ac97815679de552
aymanjameel60 <95094618+aymanjameel60@users.noreply.github.com>
2026-09-06 00:18:02 +0300

    Load Spike vendor shell v2 after Mercur styles

--- templates/basic/apps/vendor/src/main.tsx
+++ templates/basic/apps/vendor/src/main.tsx
@@ -4,6 +4,7 @@ import "@mercurjs/vendor/index.css";
 import "./spike/vendor-spike.css";
 import "./spike/spike-system-ui.css";
 import "./spike/ui-components.css";
+import "./spike/spike-shell-v2.css";
 import App from "@mercurjs/vendor";
 import ExchangeBar from "./spike/ExchangeBar";
 
