bcf29a77f1c8bbceebe0f0a21c80a4a6afb6173f
aymanjameel60 <95094618+aymanjameel60@users.noreply.github.com>
2026-09-06 01:26:57 +0300

    Apply Spike light RTL shadcn theme to vendor app

--- templates/basic/apps/vendor/src/main.tsx
+++ templates/basic/apps/vendor/src/main.tsx
@@ -5,8 +5,14 @@ import "./spike/vendor-spike.css";
 import "./spike/spike-system-ui.css";
 import "./spike/ui-components.css";
 import "./spike/spike-shell-v2.css";
+import "./index.css";
 import App from "@mercurjs/vendor";
 import ExchangeBar from "./spike/ExchangeBar";
+
+document.documentElement.lang = "ar";
+document.documentElement.dir = "rtl";
+document.documentElement.classList.remove("dark");
+document.documentElement.style.colorScheme = "light";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
